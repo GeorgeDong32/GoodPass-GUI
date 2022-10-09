432c67af18b9e4f065c471789b448fcea196c30d
GeorgeDong32 <georgedong32@foxmail.com>
2022-10-09 19:54:32 +0800

    add WinUI files

---
+++ WinUI3/GoodPass/GoodPass/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace GoodPass.ViewModels;
+
+public class MainViewModel : ObservableRecipient
+{
+    public MainViewModel()
+    {
+    }
+}
---
+++ WinUI3/GoodPass/GoodPass/ViewModels/ShellViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System.Windows.Input;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+using GoodPass.Contracts.Services;
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Navigation;
+
+namespace GoodPass.ViewModels;
+
+public class ShellViewModel : ObservableRecipient
+{
+    private bool _isBackEnabled;
+
+    public ICommand MenuFileExitCommand
+    {
+        get;
+    }
+
+    public ICommand MenuSettingsCommand
+    {
+        get;
+    }
+
+    public ICommand MenuViewsListDetailsCommand
+    {
+        get;
+    }
+
+    public ICommand MenuViewsMainCommand
+    {
+        get;
+    }
+
+    public INavigationService NavigationService
+    {
+        get;
+    }
+
+    public bool IsBackEnabled
+    {
+        get => _isBackEnabled;
+        set => SetProperty(ref _isBackEnabled, value);
+    }
+
+    public ShellViewModel(INavigationService navigationService)
+    {
+        NavigationService = navigationService;
+        NavigationService.Navigated += OnNavigated;
+
+        MenuFileExitCommand = new RelayCommand(OnMenuFileExit);
+        MenuSettingsCommand = new RelayCommand(OnMenuSettings);
+        MenuViewsListDetailsCommand = new RelayCommand(OnMenuViewsListDetails);
+        MenuViewsMainCommand = new RelayCommand(OnMenuViewsMain);
+    }
+
+    private void OnNavigated(object sender, NavigationEventArgs e) => IsBackEnabled = NavigationService.CanGoBack;
+
+    private void OnMenuFileExit() => Application.Current.Exit();
+
+    private void OnMenuSettings() => NavigationService.NavigateTo(typeof(SettingsViewModel).FullName!);
+
+    private void OnMenuViewsListDetails() => NavigationService.NavigateTo(typeof(ListDetailsViewModel).FullName!);
+
+    private void OnMenuViewsMain() => NavigationService.NavigateTo(typeof(MainViewModel).FullName!);
+}
