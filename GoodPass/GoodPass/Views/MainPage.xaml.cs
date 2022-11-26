13e7681d802632d089e557b838de67a1c6dabfc7
George·Dong <georgedong32@foxmail.com>
2022-11-26 20:01:50 +0800

    微调UI

--- GoodPass/GoodPass/Views/MainPage.xaml.cs
+++ GoodPass/GoodPass/Views/MainPage.xaml.cs
@@ -39,14 +39,14 @@ public sealed partial class MainPage : Page
         else if (MKCheck_Result == "npass")
         {
             Login_InfoBar.IsOpen = true;
-            Login_InfoBar.Background = new SolidColorBrush(Color.FromArgb(100, 255, 0, 0));//设置提示为红色
+            Login_InfoBar.Background = new SolidColorBrush(Color.FromArgb(200, 255, 0, 0));//设置提示为红色
             Login_InfoBar.Message = "密码错误，请检查后重试！";//底部横幅提示
         }
         else if (MKCheck_Result == "error: not found")
         {
             //报错：MKConfig路径不存在
             Login_InfoBar.IsOpen = true;
-            Login_InfoBar.Background = new SolidColorBrush(Color.FromArgb(100, 255, 0, 0));
+            Login_InfoBar.Background = new SolidColorBrush(Color.FromArgb(200, 255, 0, 0));
             Login_InfoBar.Message = "配置文件不存在！";
             //To Do: 添加进入设置密码界面
             ShowSetMKDialog();
@@ -55,7 +55,7 @@ public sealed partial class MainPage : Page
         {
             //报错：MKConfig数据损坏
             Login_InfoBar.IsOpen = true;
-            Login_InfoBar.Background = new SolidColorBrush(Color.FromArgb(100, 255, 0, 0));
+            Login_InfoBar.Background = new SolidColorBrush(Color.FromArgb(200, 255, 0, 0));
             Login_InfoBar.Message = "配置文件损坏，请修复！";
             //To Do: 添加进入重设密码界面
             ShowResetMKDialog();
@@ -64,7 +64,7 @@ public sealed partial class MainPage : Page
         {
             //报错：未知错误
             Login_InfoBar.IsOpen = true;
-            Login_InfoBar.Background = new SolidColorBrush(Color.FromArgb(100, 255, 0, 0));
+            Login_InfoBar.Background = new SolidColorBrush(Color.FromArgb(200, 255, 0, 0));
             Login_InfoBar.Message = "未知错误！";
         }
     }
@@ -82,14 +82,14 @@ public sealed partial class MainPage : Page
         else if (MKCheck_Result == "npass")
         {
             Login_InfoBar.IsOpen = true;
-            Login_InfoBar.Background = new SolidColorBrush(Color.FromArgb(100, 255, 0, 0));//设置提示为红色
+            Login_InfoBar.Background = new SolidColorBrush(Color.FromArgb(120, 255, 0, 0));//设置提示为红色
             Login_InfoBar.Message = "密码错误，请检查后重试！";//底部横幅提示
         }
         else if (MKCheck_Result == "error: not found")
         {
             //报错：MKConfig路径不存在
             Login_InfoBar.IsOpen = true;
-            Login_InfoBar.Background = new SolidColorBrush(Color.FromArgb(100, 255, 0, 0));
+            Login_InfoBar.Background = new SolidColorBrush(Color.FromArgb(120, 255, 0, 0));
             Login_InfoBar.Message = "配置文件不存在！";
             //To Do: 添加进入设置密码界面
             ShowSetMKDialog();
@@ -98,7 +98,7 @@ public sealed partial class MainPage : Page
         {
             //报错：MKConfig数据损坏
             Login_InfoBar.IsOpen = true;
-            Login_InfoBar.Background = new SolidColorBrush(Color.FromArgb(100, 255, 0, 0));
+            Login_InfoBar.Background = new SolidColorBrush(Color.FromArgb(120, 255, 0, 0));
             Login_InfoBar.Message = "配置文件损坏，请修复！";
             //To Do: 添加进入重设密码界面
             ShowResetMKDialog();
@@ -107,7 +107,7 @@ public sealed partial class MainPage : Page
         {
             //报错：未知错误
             Login_InfoBar.IsOpen = true;
-            Login_InfoBar.Background = new SolidColorBrush(Color.FromArgb(100, 255, 0, 0));
+            Login_InfoBar.Background = new SolidColorBrush(Color.FromArgb(120, 255, 0, 0));
             Login_InfoBar.Message = "未知错误！";
         }
     }
@@ -123,7 +123,7 @@ public sealed partial class MainPage : Page
         if (result == ContentDialogResult.Primary)
         {
             Login_InfoBar.IsOpen = true;
-            Login_InfoBar.Background = new SolidColorBrush(Color.FromArgb(50, 98, 255, 223));//设置提示为绿色
+            Login_InfoBar.Background = new SolidColorBrush(Color.FromArgb(100, 98, 255, 223));//设置提示为绿色
             Login_InfoBar.Message = "成功设置主密码！";
         }
     }
@@ -140,7 +140,7 @@ public sealed partial class MainPage : Page
         if (result == ContentDialogResult.Primary)
         {
             Login_InfoBar.IsOpen = true;
-            Login_InfoBar.Background = new SolidColorBrush(Color.FromArgb(50, 98, 255, 223));//设置提示为绿色
+            Login_InfoBar.Background = new SolidColorBrush(Color.FromArgb(100, 98, 255, 223));//设置提示为绿色
             Login_InfoBar.Message = "成功重设主密码";
         }
     }
