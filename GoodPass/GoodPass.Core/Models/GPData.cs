7962fd91d05f15f044d095beb1bb9565ff70b50c
George·Dong <georgedong32@foxmail.com>
2022-12-02 11:38:01 +0800

    Init GPData

---
+++ GoodPass/GoodPass.Core/Models/GPData.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodPass.Core.Models;
+public class GPData
+{
+    /*数据*/
+    public string PlatformName
+    {
+        get; set;
+    }
+    
+    public string PlatformUrl
+    {
+        get; set;
+    }
+
+    public string AccountName
+    {
+        get; set;
+    }
+
+    private string EncPassword
+    {
+        get; set;
+    }
+
+    public string DecPassword
+    {
+        get; set;
+    }
+
+    public DateTime LatestUpdateTime
+    {
+        get; set; 
+    }
+
+    /*方法*/
+    public void selfUpdate()
+    {
+        /*Todo:添加数据自升级的相应代码*/
+        LatestUpdateTime = DateTime.Now;
+    }
+
+    public void dataDecrypt()
+    {
+        /*Todo:添加使用GPSESservice解密的实现*/
+        DecPassword = EncPassword;
+    }
+
+    public string getPassword() => DecPassword;
+}
