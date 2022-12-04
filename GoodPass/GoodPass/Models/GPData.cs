93ec7cc289bb374a80a7251b582618036d5639c5
George·Dong <georgedong32@foxmail.com>
2022-12-04 17:10:20 +0800

    完成GoodPassData类构建

---
+++ GoodPass/GoodPass/Models/GPData.cs
@@ -0,0 +1,97 @@
+﻿using GoodPass.Services;
+
+namespace GoodPass.Models;
+
+public class GPData
+{
+    /*数据*/
+    public string PlatformName
+    {
+        get; set;
+    }
+
+    public string? PlatformUrl
+    {
+        get; set;
+    }
+
+    public string AccountName
+    {
+        get; set;
+    }
+
+    private string EncPassword
+    {
+        get; set;
+    }
+
+    private string DecPassword
+    {
+        get; set;
+    }
+
+    public DateTime LatestUpdateTime
+    {
+        get; set;
+    }
+
+    /*方法*/
+    public GPData()
+    {
+        PlatformName = "No Name";
+        PlatformUrl = null;
+        AccountName = "No Name";
+        DecPassword = "DecPassword";
+        EncPassword = DecPassword;
+        LatestUpdateTime = DateTime.Now;
+    }
+
+    public GPData(string platformName, string platformUrl, string accountName, string encPassword, DateTime latestUpdateTime)
+    {
+        PlatformName = platformName;
+        PlatformUrl = platformUrl;
+        AccountName = accountName;
+        EncPassword = encPassword;
+        DecPassword = EncPassword;
+        LatestUpdateTime = latestUpdateTime;
+    }
+
+    public void selfUpdate() //预留接口
+    {
+        /*Todo:添加数据自升级的相应代码*/
+        LatestUpdateTime = DateTime.Now;
+    }
+
+    public bool dataDecrypt()
+    {
+        var GPCS = new GoodPassCryptographicServices();
+        DecPassword = GPCS.decryptStr(EncPassword);
+        return true;
+    }
+
+    public string getPassword() => DecPassword;
+
+    public string changePassword(string newPassword)
+    {
+        dataDecrypt();
+        if (newPassword != DecPassword && newPassword != String.Empty && newPassword != null)
+        {
+            var GPCS = new GoodPassCryptographicServices();
+            DecPassword = newPassword;
+            EncPassword = GPCS.encryptStr(newPassword);
+            return "Success";
+        }
+        else if (newPassword == DecPassword)
+        {
+            return "SamePassword";
+        }
+        else if (newPassword == String.Empty || newPassword == null)
+        {
+            return "Empty";
+        }
+        else
+        {
+            return "Unknown Error";
+        }
+    }
+}
