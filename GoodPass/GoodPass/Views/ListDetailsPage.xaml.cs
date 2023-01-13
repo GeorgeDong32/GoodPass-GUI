0ea94b2ddf0fc8441110e000c9400ade716c9245
George·Dong <georgedong32@foxmail.com>
2023-01-13 22:37:42 +0800

    尝试实现删除功能2

--- GoodPass/GoodPass/Views/ListDetailsPage.xaml.cs
+++ GoodPass/GoodPass/Views/ListDetailsPage.xaml.cs
@@ -27,8 +27,5 @@ public sealed partial class ListDetailsPage : Page
         }
     }
 
-    private void ListDetailsViewControl_DataContextChanged(Microsoft.UI.Xaml.FrameworkElement sender, Microsoft.UI.Xaml.DataContextChangedEventArgs args)
-    {
-        InitializeComponent();
-    }
+    public ListDetailsViewModel GetDetailsViewModel() => ViewModel;
 }
