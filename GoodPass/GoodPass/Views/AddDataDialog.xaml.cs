6963431f3ec4d2a37092f64163bfbf19db70fc9f
George·Dong <georgedong32@foxmail.com>
2023-01-15 23:03:46 +0800

    添加添加数据弹窗

---
+++ GoodPass/GoodPass/Views/AddDataDialog.xaml.cs
@@ -0,0 +1,18 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using Microsoft.UI.Xaml.Controls;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace GoodPass.Views
+{
+    public sealed partial class AddDataDialog : UserControl
+    {
+        public AddDataDialog()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
