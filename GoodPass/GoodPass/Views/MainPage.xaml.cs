f90f54b845f4a524e3c899969520256dce9aa3ba
George·Dong <georgedong32@foxmail.com>
2023-01-29 17:26:32 +0800

    优化解锁函数性能

--- GoodPass/GoodPass/Views/MainPage.xaml.cs
+++ GoodPass/GoodPass/Views/MainPage.xaml.cs
@@ -4,7 +4,6 @@ using GoodPass.ViewModels;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media;
-using Windows.Security.Cryptography.Certificates;
 using Windows.UI;
 
 namespace GoodPass.Views;
@@ -74,47 +73,8 @@ public sealed partial class MainPage : Page
 
     private async void Login_Check_ClickAsync(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
     {
-        var passwordInput = Login_PssswordBox.Password;
-        var MKCheck_Result = await MKS.CheckMasterKeyAsync(passwordInput);
-        App.DataManager = new Models.GPManager();
-        //添加解锁逻辑
-        if (MKCheck_Result == "pass")
-        {
-            App.App_UnLock();
-            App.DataManager.LoadFormFile($"C:\\Users\\{Environment.UserName}\\AppData\\Local\\GoodPass\\GoodPassData.csv");
-            ViewModel.Login_UnLock();
-        }
-        else if (MKCheck_Result == "npass")
-        {
-            Login_InfoBar.IsOpen = true;
-            Login_InfoBar.Background = new SolidColorBrush(Color.FromArgb(120, 255, 0, 0));//设置提示为红色
-            Login_InfoBar.Message = "密码错误，请检查后重试！";//底部横幅提示
-        }
-        else if (MKCheck_Result == "error: not found")
-        {
-            //报错：MKConfig路径不存在
-            Login_InfoBar.IsOpen = true;
-            Login_InfoBar.Background = new SolidColorBrush(Color.FromArgb(120, 255, 0, 0));
-            Login_InfoBar.Message = "配置文件不存在！";
-            //To Do: 添加进入设置密码界面
-            ShowSetMKDialog();
-        }
-        else if (MKCheck_Result == "error: data broken")
-        {
-            //报错：MKConfig数据损坏
-            Login_InfoBar.IsOpen = true;
-            Login_InfoBar.Background = new SolidColorBrush(Color.FromArgb(120, 255, 0, 0));
-            Login_InfoBar.Message = "配置文件损坏，请修复！";
-            //To Do: 添加进入重设密码界面
-            ShowResetMKDialog();
-        }
-        else
-        {
-            //报错：未知错误
-            Login_InfoBar.IsOpen = true;
-            Login_InfoBar.Background = new SolidColorBrush(Color.FromArgb(120, 255, 0, 0));
-            Login_InfoBar.Message = "未知错误！";
-        }
+        UnlockProcess();
+        await Task.CompletedTask;
     }
 
     private async void ShowSetMKDialog()//密码设置弹窗
@@ -161,47 +121,54 @@ public sealed partial class MainPage : Page
     {
         if (e.Key == Windows.System.VirtualKey.Enter)
         {
-            var passwordInput = Login_PssswordBox.Password;
-            var MKCheck_Result = await MKS.CheckMasterKeyAsync(passwordInput);
-            App.DataManager = new Models.GPManager();
-            //添加解锁逻辑
-            if (MKCheck_Result == "pass")
-            {
-                App.App_UnLock();
-                App.DataManager.LoadFormFile($"C:\\Users\\{Environment.UserName}\\AppData\\Local\\GoodPass\\GoodPassData.csv");
-                ViewModel.Login_UnLock();
-            }
-            else if (MKCheck_Result == "npass")
-            {
-                Login_InfoBar.IsOpen = true;
-                Login_InfoBar.Background = new SolidColorBrush(Color.FromArgb(120, 255, 0, 0));//设置提示为红色
-                Login_InfoBar.Message = "密码错误，请检查后重试！";//底部横幅提示
-            }
-            else if (MKCheck_Result == "error: not found")
-            {
-                //报错：MKConfig路径不存在
-                Login_InfoBar.IsOpen = true;
-                Login_InfoBar.Background = new SolidColorBrush(Color.FromArgb(120, 255, 0, 0));
-                Login_InfoBar.Message = "配置文件不存在！";
-                //To Do: 添加进入设置密码界面
-                ShowSetMKDialog();
-            }
-            else if (MKCheck_Result == "error: data broken")
-            {
-                //报错：MKConfig数据损坏
-                Login_InfoBar.IsOpen = true;
-                Login_InfoBar.Background = new SolidColorBrush(Color.FromArgb(120, 255, 0, 0));
-                Login_InfoBar.Message = "配置文件损坏，请修复！";
-                //To Do: 添加进入重设密码界面
-                ShowResetMKDialog();
-            }
-            else
-            {
-                //报错：未知错误
-                Login_InfoBar.IsOpen = true;
-                Login_InfoBar.Background = new SolidColorBrush(Color.FromArgb(120, 255, 0, 0));
-                Login_InfoBar.Message = "未知错误！";
-            }
+            UnlockProcess();
+            await Task.CompletedTask;
+        }
+    }
+
+    private async void UnlockProcess()
+    {
+        var passwordInput = Login_PssswordBox.Password;
+        var MKCheck_Result = await MKS.CheckMasterKeyAsync(passwordInput);
+        App.DataManager ??= new Models.GPManager(); //为null时才赋值
+        //添加解锁逻辑
+        if (MKCheck_Result == "pass")
+        {
+            App.App_UnLock();
+            App.DataManager.LoadFormFile($"C:\\Users\\{Environment.UserName}\\AppData\\Local\\GoodPass\\GoodPassData.csv");
+            App.DataManager.DecryptAllDatas();
+            ViewModel.Login_UnLock();
+        }
+        else if (MKCheck_Result == "npass")
+        {
+            Login_InfoBar.IsOpen = true;
+            Login_InfoBar.Background = new SolidColorBrush(Color.FromArgb(120, 255, 0, 0));//设置提示为红色
+            Login_InfoBar.Message = "密码错误，请检查后重试！";//底部横幅提示
+        }
+        else if (MKCheck_Result == "error: not found")
+        {
+            //报错：MKConfig路径不存在
+            Login_InfoBar.IsOpen = true;
+            Login_InfoBar.Background = new SolidColorBrush(Color.FromArgb(120, 255, 0, 0));
+            Login_InfoBar.Message = "配置文件不存在！";
+            //To Do: 添加进入设置密码界面
+            ShowSetMKDialog();
+        }
+        else if (MKCheck_Result == "error: data broken")
+        {
+            //报错：MKConfig数据损坏
+            Login_InfoBar.IsOpen = true;
+            Login_InfoBar.Background = new SolidColorBrush(Color.FromArgb(120, 255, 0, 0));
+            Login_InfoBar.Message = "配置文件损坏，请修复！";
+            //To Do: 添加进入重设密码界面
+            ShowResetMKDialog();
+        }
+        else
+        {
+            //报错：未知错误
+            Login_InfoBar.IsOpen = true;
+            Login_InfoBar.Background = new SolidColorBrush(Color.FromArgb(120, 255, 0, 0));
+            Login_InfoBar.Message = "未知错误！";
         }
     }
 }
