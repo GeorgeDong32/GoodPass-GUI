d08dc6501c71516158d45771566bfe49f1e61991
George·Dong <georgedong32@foxmail.com>
2023-02-04 23:25:37 +0800

    构建OOBE

---
+++ GoodPass/GoodPass/Services/GoodPassOOBE.cs
@@ -0,0 +1,18 @@
+﻿namespace GoodPass.Services;
+
+/// <summary>
+/// Provide GoodPass Out-Of-Box-Experience Services
+/// </summary>
+public class OOBEServices
+{
+    private const string oobeSettingKey = "OOBE";
+
+    private readonly LocalSettingsService _localSettingsService;
+
+    public OOBEServices()
+    {
+        _localSettingsService = App.GetService<LocalSettingsService>();
+    }
+
+    
+}
