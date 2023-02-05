e9a47494306b1213ef25fb1ed5332c51df288f7a
George·Dong <georgedong32@foxmail.com>
2023-02-05 23:05:21 +0800

    完善OOBE Services

--- GoodPass/GoodPass/Services/GoodPassOOBE.cs
+++ GoodPass/GoodPass/Services/GoodPassOOBE.cs
@@ -1,4 +1,7 @@
-﻿namespace GoodPass.Services;
+﻿using GoodPass.Contracts.Services;
+using GoodPass.Models;
+
+namespace GoodPass.Services;
 
 /// <summary>
 /// Provide GoodPass Out-Of-Box-Experience Services
@@ -7,12 +10,49 @@ public class OOBEServices
 {
     private const string oobeSettingKey = "OOBE";
 
-    private readonly LocalSettingsService _localSettingsService;
+    private readonly ILocalSettingsService _localSettingsService;
 
-    public OOBEServices()
+    private OOBESituation _OOBESituation
     {
-        _localSettingsService = App.GetService<LocalSettingsService>();
+        get; set; 
     }
 
-    
+    public OOBEServices(ILocalSettingsService localSettingsService)
+    {
+        _localSettingsService = localSettingsService;
+    }
+
+    public async Task<OOBESituation> GetOOBEStatusAsync()
+    {
+        var loaclstatus = await _localSettingsService.ReadSettingAsync<string>(oobeSettingKey);
+        switch (loaclstatus)
+        {
+            case "Enable":
+                _OOBESituation = OOBESituation.EnableOOBE;
+                break;
+            case "Disable":
+                _OOBESituation = OOBESituation.DIsableOOBE;
+                break;
+            default:
+                _OOBESituation = OOBESituation.EnableOOBE;
+                break;
+        }
+        return _OOBESituation;
+    }
+
+    public async Task<bool> SetOOBEStatusAsync(OOBESituation oobeSituation)
+    {
+        _OOBESituation = oobeSituation;
+        switch (oobeSituation)
+        {
+            case OOBESituation.EnableOOBE:
+                await _localSettingsService.SaveSettingAsync(oobeSettingKey, "Enable");
+                return true;
+            case OOBESituation.DIsableOOBE:
+                await _localSettingsService.SaveSettingAsync(oobeSettingKey, "Disable");
+                return true;
+            default:
+                return false;
+        }
+    }
 }
