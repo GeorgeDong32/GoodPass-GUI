65523bf4f06aaec9281dd0aa8d1b86602598b2e1
George·Dong <georgedong32@foxmail.com>
2023-04-15 21:55:33 +0800

    尝试多语言

---
+++ GoodPass/GoodPass/Models/LanguageManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Globalization;
+
+namespace GoodPass.Models;
+
+public class Language
+{
+    #region Properties
+    public CultureInfo Culture
+    {
+        get;
+    }
+
+    public string InteralName
+    {
+        get;
+    }
+
+    public string DisplayName
+    {
+        get;
+    }
+
+    #endregion
+
+    #region Methods
+
+    public Language()
+    {
+        Culture = new CultureInfo(Windows.System.UserProfile.GlobalizationPreferences.Languages[0]);
+        DisplayName = Culture.DisplayName;
+        InteralName = Culture.Name;
+    }
+
+    public Language(CultureInfo cultureInfo)
+    {
+        Culture = cultureInfo;
+        DisplayName = Culture.DisplayName;
+        InteralName = Culture.Name;
+    }
+
+    #endregion
+}
+public class LanguageManager
+{
+    #region Properties
+
+    public List<Language> AvaliableLanguages
+    {
+        get;
+    }
+
+    public Language CurrentLanguage
+    {
+        get;
+    }
+
+    #endregion
+
+    #region Methods
+
+    public LanguageManager()
+    {
+        AvaliableLanguages = new List<Language>();
+        AvaliableLanguages.Add(new Language(new CultureInfo("en-US")));
+        AvaliableLanguages.Add(new Language(new CultureInfo("zh-CN")));
+        CurrentLanguage = new Language();
+    }
+
+    #endregion
+}
